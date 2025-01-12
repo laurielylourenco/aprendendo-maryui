1f7309ce6c850341d0bcc6859156762998c1314a
Lauriely Lourenço <laurielylourenco@gmail.com>
2025-01-12 20:19:23 -0300

    setup,listagem de usuarios e paginacao

---
+++ resources/views/livewire/users/index.blade.php
@@ -0,0 +1,123 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use Livewire\Volt\Component;
+use Mary\Traits\Toast;
+use Livewire\WithPagination;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+new class extends Component {
+    use Toast;
+    use WithPagination;
+
+    public string $search = '';
+
+    public bool $drawer = false;
+
+    public array $sortBy = ['column' => 'name', 'direction' => 'asc'];
+
+    // Clear filters
+    public function clear(): void
+    {
+        $this->reset();
+        $this->resetPage();
+        $this->success('Filters cleared.', position: 'toast-bottom');
+    }
+
+    // Delete action
+/*     public function delete($id): void
+    {
+        $this->warning("Will delete #$id", 'It is fake.', position: 'toast-bottom');
+    } */
+
+    public function delete(User $user): void
+    {
+        $user->delete();
+        $this->warning("$user->name deleted", 'Good bye!', position: 'toast-bottom');
+    }
+
+    // Table headers
+    public function headers(): array
+    {
+        return [
+            ['key' => 'id', 'label' => '#', 'class' => 'w-1'],
+            ['key' => 'name', 'label' => 'Name', 'class' => 'w-64'],
+            ['key' => 'country_name', 'label' => 'Country', 'class' => 'w-20'],
+            ['key' => 'email', 'label' => 'E-mail', 'sortable' => false],
+        ];
+    }
+
+    /**
+     * For demo purpose, this is a static collection.
+     *
+     * On real projects you do it with Eloquent collections.
+     * Please, refer to maryUI docs to see the eloquent examples.
+     */
+    public function users(): LengthAwarePaginator
+    {
+        /*   return collect([
+            ['id' => 1, 'name' => 'Mary', 'email' => 'mary@mary-ui.com', 'age' => 23],
+            ['id' => 2, 'name' => 'Giovanna', 'email' => 'giovanna@mary-ui.com', 'age' => 7],
+            ['id' => 3, 'name' => 'Marina', 'email' => 'marina@mary-ui.com', 'age' => 5],
+        ])
+            ->sortBy([[...array_values($this->sortBy)]])
+            ->when($this->search, function (Collection $collection) {
+                return $collection->filter(fn(array $item) => str($item['name'])->contains($this->search, true));
+            }); */
+
+        return User::query()
+            ->with(['country'])
+            ->withAggregate('country', 'name')
+            ->when($this->search, fn(Builder $q) => $q->where('name', 'like', "%$this->search%"))
+            ->orderBy(...array_values($this->sortBy))
+            ->paginate(5);
+    }
+
+    public function updated($property): void
+    {
+        if (! is_array($property) && $property != "") {
+            $this->resetPage();
+        }
+    }
+
+    public function with(): array
+    {
+        return [
+            'users' => $this->users(),
+            'headers' => $this->headers()
+        ];
+    }
+}; ?>
+
+<div>
+    <!-- HEADER -->
+    <x-header title="Users" separator progress-indicator>
+        <x-slot:middle class="!justify-end">
+            <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
+        </x-slot:middle>
+        <x-slot:actions>
+            <x-button label="Filters" @click="$wire.drawer = true" responsive icon="o-funnel" />
+        </x-slot:actions>
+    </x-header>
+
+    <!-- TABLE  -->
+    <x-card>
+        <x-table :headers="$headers" :rows="$users" :sort-by="$sortBy" ... with-pagination>
+            @scope('actions', $user)
+            <x-button icon="o-trash" wire:click="delete({{ $user['id'] }})" wire:confirm="Are you sure?" spinner class="btn-ghost btn-sm text-red-500" />
+            @endscope
+        </x-table>
+    </x-card>
+
+    <!-- FILTER DRAWER -->
+    <x-drawer wire:model="drawer" title="Filters" right separator with-close-button class="lg:w-1/3">
+        <x-input placeholder="Search..." wire:model.live.debounce="search" icon="o-magnifying-glass" @keydown.enter="$wire.drawer = false" />
+
+        <x-slot:actions>
+            <x-button label="Reset" icon="o-x-mark" wire:click="clear" spinner />
+            <x-button label="Done" icon="o-check" class="btn-primary" @click="$wire.drawer = false" />
+        </x-slot:actions>
+    </x-drawer>
+</div>
